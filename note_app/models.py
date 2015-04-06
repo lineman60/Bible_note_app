0a4286d44e32a09d716f80ce9892615335faeb4d
Beryl <bsnyder@nucleoware.com>
2015-04-06 09:40:45 -0500

    starting to create model

--- note_app/models.py
+++ note_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Anote(models.Model):
+    book = models.CharField(max_length=60)
+    chapter = models.IntegerField()
+    verse = models.IntegerField()
+    note_text = models.TextField(max_length=400)
+    note_source = models.CharField(max_length=60)
