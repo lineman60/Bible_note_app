620265abf656ba731afab0b8732230cc550ae5e2
Beryl <bsnyder@nucleoware.com>
2015-04-21 12:43:46 -0500

    Adding login requirments

--- note_app/views.py
+++ note_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 from note_app.models import Anote
 from note_app.forms import NoteForm
 
@@ -20,16 +21,23 @@ def add_note(request):
         form = NoteForm()
     return  render(request, 'add_note.html', {'form': form})
 
-def edit_note(request,  id=None):
-    # if id:
-    # if request.method == 'POST':
+@login_required
+def edit_note(request, id=None):
     temp_note = get_object_or_404(Anote,pk=id)
-        # if Anote.contributor = request.user:
-        #     raise HttpResponseForbidden()
-        # else:
-        #     form = NoteForm(instance=temp_note)
-    form = NoteForm(instance=temp_note)
-    return render(request, 'edit_note.html', {'form': form})
+    if id:
+    # if request.method == 'POST':
+        #if Anote.contributor != request.user:
+        if temp_note.contributor != request.user:
+            print(request.user)
+            print(temp_note.contributor)
+            return HttpResponseForbidden()
+        else:
+             form = NoteForm(instance=temp_note)
+             if form.is_valid():
+                form.save(commit=True)
+             else:
+                print form.errors
+        return render(request, 'edit_note.html', {'form': form})
 
 
 
