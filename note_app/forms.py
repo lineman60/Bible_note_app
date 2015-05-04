50c04fb12742f399f3acf11716cfed795084af86
Beryl <bsnyder@nucleoware.com>
2015-05-04 06:17:37 -0500

    Books of the bible are now in dropdown vs typein

--- note_app/forms.py
+++ note_app/forms.py
@@ -5,16 +5,12 @@ from note_app.models import Anote, BookName
 
 class NoteForm(forms.ModelForm):
         book = forms.ChoiceField(BookName.books_of_the_bible_choices,help_text="Book")
-        # book = forms.CharField(max_length=60, help_text="Book")
         chapter = forms.IntegerField(help_text="Chapter")
         verse = forms.IntegerField(help_text="Verse")
         note_text = forms.CharField(widget=forms.Textarea, max_length=400, help_text="Note")
         note_source = forms.CharField(max_length=60, help_text="Source", required=False)
         is_public = forms.BooleanField(help_text="Make the Note Public", required=False)
-        # contributor = forms.CharField(max_length=128, help_text="User Name")
-        # contributor = forms.CharField(max_length=128,)
         contributor = forms.CharField(widget=forms.HiddenInput(),)
 
         class Meta:
             model = Anote
-            # exclude = ('contributor',)
